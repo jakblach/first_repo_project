9cc8c463275772d8ea6167f99239e38f527684a2
jakub <faketaxi@gamil.com>
2024-06-29 14:38:03 +0200

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        SUV fortuner = new SUV("Fortuner", false );
+        fortuner.move(40,0);
+        fortuner.accelerate(20);
+//        fortuner.accelerate(-60);
+        System.out.println("Current gear : "+ fortuner.getCurrentGear());
+        System.out.println("Current gear : "+ fortuner.getCurrentSpeed());
+
+    }
+}
